13efa1e05d0b8777dd66789c82ba5b139686f4a8
Wan Bachtiar <sindbach@gmail.com>
2022-04-20 11:16:10 +1000

    Simplify wrapper script

--- get-started.sh
+++ get-started.sh
@@ -7,16 +7,13 @@ then
     read -p "MONGODB URI (Required): " MONGODB_URI
 fi
 
-COMMAND="npm config set cache /workspace/.npm --global;"
-if [ -z ${2} ]
+if ! [ -z ${2} ]
 then
-    COMMAND=${COMMAND}
-else
     COMMAND=${COMMAND}"npm install mongodb@${2} --save;"
 fi
 COMMAND=${COMMAND}"node ./getstarted.js"
 
-HOME=/home/gsuser
+HOME=/workspace
 echo "Executing ... "${COMMAND}
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":${HOME} \
